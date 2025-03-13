c975d1316f8638a316dd0ef04df9a41d1bfac891
Harsh Darji <darjiharsh2005@gmail.com>
2025-03-13 19:53:09 +0530

    updated file

--- config/db.js
+++ config/db.js
@@ -1,13 +1,16 @@
-require('dotenv').config(); // This will load environment variables from the .env file
-const mysql = require('mysql2');
+const { Sequelize } = require("sequelize");
 
-// Create a connection pool to the database using environment variables
-const db = mysql.createPool({
-    host: process.env.DB_HOST,           // Database host
-    user: process.env.DB_USER,           // Database user
-    password: process.env.DB_PASSWORD,   // Database password
-    database: process.env.DB_NAME,       // Database name
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  dialect: "mysql",
+  port: process.env.DB_PORT || 3306,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // Add this if you're using a managed database that requires SSL
+    }
+  },
+  logging: false, // Disable logging to clean up console output
 });
 
-// Export the connection pool to be used in other files
-module.exports = db.promise();
+module.exports = sequelize;
