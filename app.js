7ac532493d84f28bd676e4334ccb5f812acee1f4
Harsh Darji <darjiharsh2005@gmail.com>
2025-04-22 19:18:08 +0530

    all done

--- app.js
+++ app.js
@@ -762,13 +762,16 @@ app.post("/send-reset-link", async (req, res) => {
       `,
     };
 
-    const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-      },
-    });
+    console.log('Email User:', process.env.EMAIL_USER);
+    console.log('Email Pass:', process.env.EMAIL_PASS);
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
+  },
+});
 
     await transporter.sendMail(mailOptions);
 
